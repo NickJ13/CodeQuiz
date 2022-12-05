3862b36d4b22ad6646fa1b63106df434e127debc
Nick Stallard <stallardlax13@gmail.com>
2022-12-05 16:18:08 -0500

    saving chnages

--- codequiz/assets/js/script.js
+++ codequiz/assets/js/script.js
@@ -3,7 +3,8 @@ var timerSpan = document.getElementById("timerspan");
 var mainEl = document.querySelector("main");
 var buttons = document.querySelectorAll("button");
 var time = 60;
-let i = 0;
+var currentQuestionIndex = 0;
+var uChoices = document.getElementById("userChoices");
 
 // you might store your questions and answers in an array, i.e. a list
 // and then you cycle through that list, displaying a question and its 4 choices
@@ -12,48 +13,33 @@ let i = 0;
 var questionsArray = [
   {
     question: "A name of storage location",
-    choice1: "variable",
-    choice2: "array",
-    choice3: "for loop",
-    choice4: "operator",
-    correctChoice: "choice1",
+    choice: ["operator","array","variable","function"], 
+    correctChoice: "array",
   },
   {
     question: "A block of code designed to perform a particular task.",
-    choice1: "variable",
-    choice2: "function",
-    choice3: "for loop",
-    choice4: "syntax",
-    correctChoice: "choice2",
+    choice: ["variable", "function", "for loop", "syntax"],
+    correctChoice: "function",
   },
   {
-    question: "A block of code designed to perform a particular task.",
-    choice1: "variable",
-    choice2: "function",
-    choice3: "for loop",
-    choice4: "syntax",
-    correctChoice: "choice2",
+    question: "loops through a block of code a number of times",
+    choice: ["variable", "function", "for loop", "syntax"],
+    correctChoice: "for loop",
   },
   {
     question: 'What is this called? "++"',
-    choice1: "decrement",
-    choice2: "modulus",
-    choice3: "addition",
-    choice4: "increment",
-    correctChoice: "choice4",
+    choice: ["decrement", "modulus", "addition", "increment"],
+    correctChoice: "increment",
   },
   {
     question: "Which of these are JavaScript operators",
-    choice1: "Arithmetic",
-    choice2: "Logical",
-    choice3: "Type",
-    choice4: "All of these above",
-    correctChoice: "choice4",
+    choice: ["Arithmetic", "Logical", "Type", "All of these above"],
+    correctChoice: "All of these above",
   },
 ];
 
-// In this case, I would list the goals like this:
-// 1. display the first question !
+
+
 // 2. Allow the user to choose an answer to the question
 // 3. determine if that choice is correct or not
 // 4. if correct, write code that accounts for a correct answer
@@ -61,30 +47,37 @@ var questionsArray = [
 // 6. Once both of those are done, call your function to render the next question
 
 function renderQuestionToScreen(questionObject) {
+  console.log('renderQuestionToScreen');
   var questionHeader = document.createElement("h3");
+  console.log(questionHeader);
   questionHeader.textContent = questionObject.question;
   var questionList = document.createElement("ul");
+  // console.log(questionObject);
+  for (var i = 0; i < questionObject.choice.length; i++) {
+    var choiceBtn = document.createElement("button");
+    choiceBtn.setAttribute('style', 'display:block;');
+    console.log(questionObject.choice[i]);
+    choiceBtn.textContent = questionObject.choice[i];
+    console.log(questionList);
+    questionList.append(choiceBtn);
+    console.log(questionList);
 
-  for (let i = 1; i <= 4; i++) {
-    var choice = document.createElement("button");
-    choice.textContent = questionObject[`choice${i}`];
-    questionList.append(choice);
   }
   mainEl.append(questionHeader, questionList);
+  console.log(mainEl);
 }
 
 startQuizBtn.addEventListener("click", function () {
   console.log("startbtnwasclicked");
-  mainEl.replaceChildren();
-  renderQuestionToScreen(questionsArray[i]);
-  // for(let i = 0; i < questionsArray.length - 1; i++) {
-  //     // renderQuestionToScreen(questionsArray[i]);
-  // }
+  startTimer();
+  // console.log(mainEl);
+  // mainEl.replaceChildren();
+  // console.log(mainEl);
+  console.log(questionsArray[currentQuestionIndex]);
+  renderQuestionToScreen(questionsArray[currentQuestionIndex]);
 });
 
-startQuizBtn.addEventListener("click", startGame);
-
-function startGame() {
+function startTimer() {
   var intervalId = setInterval(function () {
     console.log(`Time left is ${time} sec`);
     timerSpan.textContent = time;
@@ -93,16 +86,10 @@ function startGame() {
       clearInterval(intervalId);
     }
   }, 1000);
-  console.log("startbtnwasclicked");
-  mainEl.replaceChildren();
-  renderQuestionToScreen(questionsArray[i]);
-  var button1 = document.createElement("button");
-  var button2 = document.createElement("button");
-  var button3 = document.createElement("button");
-  var button4 = document.createElement("button");
-  button1.textContent = questionsArray[1].choice1;
-  button2.textContent = questionsArray[2].choice2;
-  button3.textContent = questionsArray[3].choice3;
-  button4.textContent = questionsArray[4].choice4;
+
   
-}
+};
+
+
+
+
