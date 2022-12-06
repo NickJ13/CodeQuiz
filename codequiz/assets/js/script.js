d313b23280fab2a2a1ee70114c8319a3885030d2
Nick Stallard <stallardlax13@gmail.com>
2022-12-05 22:31:38 -0500

    save

--- codequiz/assets/js/script.js
+++ codequiz/assets/js/script.js
@@ -6,11 +6,10 @@ var time = 60;
 var currentQuestionIndex = 0;
 var uChoices = document.getElementById("userChoices");
 
-
 var questionsArray = [
   {
     question: "A name of storage location",
-    choice: ["operator","array","variable","function"], 
+    choice: ["operator", "array", "variable", "function"],
     correctChoice: "array",
   },
   {
@@ -35,8 +34,6 @@ var questionsArray = [
   },
 ];
 
-
-
 // 2. Allow the user to choose an answer to the question
 // 3. determine if that choice is correct or not
 // 4. if correct, write code that accounts for a correct answer
@@ -44,7 +41,7 @@ var questionsArray = [
 // 6. Once both of those are done, call your function to render the next question
 
 function renderQuestionToScreen(questionObject) {
-  console.log('renderQuestionToScreen');
+  console.log("renderQuestionToScreen");
   var questionHeader = document.createElement("h3");
   console.log(questionHeader);
   questionHeader.textContent = questionObject.question;
@@ -52,13 +49,14 @@ function renderQuestionToScreen(questionObject) {
   // console.log(questionObject);
   for (var i = 0; i < questionObject.choice.length; i++) {
     var choiceBtn = document.createElement("button");
-    choiceBtn.setAttribute('style', 'display:block;');
+    choiceBtn.setAttribute("style", "display:block;");
     console.log(questionObject.choice[i]);
     choiceBtn.textContent = questionObject.choice[i];
     // console.log(questionList);
     questionList.append(choiceBtn);
     // console.log(questionList);
-
+    choiceBtn.setAttribute("value", questionObject.choice[i]);
+    choiceBtn.onclick = checkAnswer;
   }
   mainEl.append(questionHeader, questionList);
   console.log(mainEl);
@@ -82,16 +80,26 @@ function startTimer() {
     if (time <= 0) {
       clearInterval(intervalId);
     }
-  }, 1000); 
-};
-
-function ifCorrect() {
-  for (var i = 0; i < questionsArray.question.length; i++) {
-    var question
-  }
+  }, 1000);
 }
 
+function checkAnswer() {
 
+  if (this.value !== questionsArray[currentQuestionIndex].answer) {
+    timer -= 15;
+    var wrongAnswer = document.createElement("h2")
+    wrongAnswer.innerHTML = "Wrong!";
+    wrongAnswer.className = "feedback-answer"
+    //yourfeedbackanswerHTMLelement.appendChild(wrongAnswer);
+  }
+  //if statement for correct answer
+
+  // if statement check time is <=0, handle ending quiz
+
+  // increase current questionIndex
+  // if statement to check if the current q index === the length of array end the quiz 
+  // else render next question
+}
 
 // 1. iterate each question in your questions array
 // 2. for each iteration, you need to display the question, then the answer choices
@@ -99,4 +107,3 @@ function ifCorrect() {
 // 4. the helper function mentioned above needs to check the selected answer and compare against the correct answer
 // 5. use a if else to determine what the code should do from step 4.  (if correct, add points.  If not, deduct time)
 // 6. move to next question and repeat
-
